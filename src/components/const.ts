0594ba9046b2d2f7d6c6f9ef2f645e6207dddb16
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-05-27 14:52:29 +0400

    feat :draw first page

--- src/components/const.ts
+++ src/components/const.ts
@@ -9,3 +9,27 @@ export let staff = "staff";
 export let article = "watch your favorite animal online";
 export let articleText = "Explore the exciting and mysterious world of wild animals in a natural setting without leaving your home.";
 export let mapBUttonText = "view live cam";
+export let donationHead = "pay and feed";
+
+//donation
+export let donation_1 = {
+  number: "01",
+  article: "Your donation has an impact",
+  text: "Providing our animals with high-quality nutritious diets is just one element of animal care at our Zoo. We do all the best so that our animals can eat food similar to what they might find in their natural habitats while making sure they get the right mix of nutrients, proteins, and vitamins to be happy and healthy. Please help us provide nutritious food for our animals by donating.The generosity of people like you can help us make sure that our animals enjoy good food that keeps them in great condition."
+};
+
+export let donation_2 = {
+  number: "02",
+  article: "Make a donation",
+  text: "You can donate through your credit card without any fees. It is easy and safe. We do not keep donors' personal information on an online network. Choose an amount to give and the pet's name if needed. of the most effective ways you can give is by making regular donations."
+};
+
+export let donation_3 = {
+  number: "03",
+  article: "Bring your food charity — straight to your favorites pets.",
+  text: "After your donation, the animal receives its favorite foods. You can support your favorite animals or any animal you care about and make a real personal impact. Never doubt that your donation can make a difference even if it is small."
+};
+
+export let donationButton = "donete now";
+export let careText = "CARE FOR THE ANIMALS YOU LOVE";
+
