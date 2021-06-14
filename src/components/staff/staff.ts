b23d139046407a99517bf0027e9c90131c116ac4
Maratkanova Alexandra <ola.maratkanova@gmail.com>
2021-06-14 20:26:52 +0400

    feat: draw staff

---
+++ src/components/staff/staff.ts
@@ -0,0 +1,28 @@
+import { createAndAppendHtmlElement } from "../utils/add-element-function";
+
+
+export class Staff {
+    private _wrapper: HTMLElement;
+
+    init() {
+        this._wrapper = document.createElement("div");
+        this._wrapper.classList.add("staff");
+
+        const BUTTONS = createAndAppendHtmlElement(this._wrapper, "div", "staff__buttons");
+        createAndAppendHtmlElement(BUTTONS, "button", "staff__button", "Persons");
+        createAndAppendHtmlElement(BUTTONS, "button", "staff__button", "Fonds");
+        createAndAppendHtmlElement(BUTTONS, "button", "staff__button", "Donations");
+        createAndAppendHtmlElement(BUTTONS, "button", "staff__button", "Messeges");
+
+        const PERSON_ACTIVITY = createAndAppendHtmlElement(this._wrapper, "div", "staff__person");
+        createAndAppendHtmlElement(PERSON_ACTIVITY, "p", "staff__person-text", "Choose a person");
+        createAndAppendHtmlElement(PERSON_ACTIVITY, "input", "staff__person-input");
+        createAndAppendHtmlElement(PERSON_ACTIVITY, "button", "staff__person-button","find");
+
+        createAndAppendHtmlElement(this._wrapper, "div", "staff__output");
+    }
+
+    get staff(): HTMLElement {
+        return this._wrapper;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,21 @@ import "./styles/main.scss";
 import { Header } from "./components/header/header";
 import { FirstPage } from "./components/first-page/first-page";
 import { Footer } from "./components/footer/footer";
-import { Messege } from "./components/pop-up/messege";
-import { Donation } from "./components/pop-up/donation";
+import { Staff } from "./components/staff/staff";
  
 const ROOT = document.querySelector(".root");
 const HEADER = new Header;
 HEADER.init();
 
-const FIRSTPAGE = new FirstPage();
-FIRSTPAGE.init();
+const STAFF = new Staff();
+STAFF.init();
+// const FIRSTPAGE = new FirstPage();
+// FIRSTPAGE.init();
 
 const FOOTER = new Footer();
 FOOTER.init();
 
 ROOT.appendChild(HEADER.header);
-ROOT.appendChild(FIRSTPAGE.firstPage);
+ROOT.appendChild(STAFF.staff);
+// ROOT.appendChild(FIRSTPAGE.firstPage);
 ROOT.appendChild(FOOTER.footer);
